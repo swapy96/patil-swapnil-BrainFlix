8f4b36a9f32d1d23c89285c0e9afd8fafc7ce89d
swapy96 <s.swapnil80@gmail.com>
2023-03-04 19:21:13 -0800

    creates folder structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <Header>
+        <div className='logo'>logo</div>
+        <div class='container'>
+            <div class='search'>
+            <div>input</div>
+            <div id='btn'>button</div>
+      </div>
+      <div class='avatar'>avatar</div>
+</div>
+
+
+    </Header>
+  )
+}
+
+export default Header
